b962e5e8d0d35142f6b3502649f4e5eeba4cc1cc
Mikhail Samylov <134856751+samylovma@users.noreply.github.com>
2024-10-27 05:34:41 +0300

    refactor

--- src/components/SubjectCard/SubjectCard.tsx
+++ src/components/SubjectCard/SubjectCard.tsx
@@ -2,16 +2,20 @@ import {SubjectModel} from '@/models/SubjectModel';
 import {Card} from '@gravity-ui/uikit';
 
 export default function SubjectCard(subject: SubjectModel) {
-    const style = {
-        width: '120px',
-        height: '120px',
-        display: 'flex',
-        alignItems: 'center',
-        justifyContent: 'center',
-    };
     return (
         <a href={`/subjects/${subject.id}`}>
-            <Card style={style} view="raised" type="container" size="l">
+            <Card
+                style={{
+                    width: '120px',
+                    height: '120px',
+                    display: 'flex',
+                    alignItems: 'center',
+                    justifyContent: 'center',
+                }}
+                view="raised"
+                type="container"
+                size="l"
+            >
                 {subject.name}
             </Card>
         </a>
