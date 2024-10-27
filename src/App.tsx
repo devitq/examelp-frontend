7352160e08a20b57de69f4e9551db1497815980f
ITQ <itq.dev@ya.ru>
2024-10-27 13:00:22 +0300

    init: Initial commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,55 @@
+import React from 'react';
+import {useNavigate} from 'react-router-dom';
+
 import {AsideHeader} from '@gravity-ui/navigation';
-import {Ghost} from '@gravity-ui/icons';
+import {ChartColumn, GraduationCap, LayoutList} from '@gravity-ui/icons';
 
 import {InfoButtons} from './components/InfoButtons';
 import {Wrapper} from './components/Wrapper';
 
 const App = () => {
+    const [compact, setCompact] = React.useState(false);
+    const navigate = useNavigate();
+
     return (
-        <AsideHeader
-            logo={{icon: Ghost, text: 'vite-example'}}
-            compact={true}
-            hideCollapseButton={true}
-            renderContent={() => (
-                <Wrapper>
-                    <InfoButtons />
-                </Wrapper>
-            )}
-        />
+        <Routes>
+            <AsideHeader
+                logo={{icon: GraduationCap, text: 'LMS'}}
+                compact={compact}
+                multipleTooltip={true}
+                headerDecoration={true}
+                onChangeCompact={(v) => {
+                    setCompact(v);
+                }}
+                subheaderItems={[
+                    {
+                        item: {
+                            id: 'all_subjects',
+                            title: 'Предметы',
+                            icon: LayoutList,
+                            link: '/',
+                            onItemClick: navigate('/subjects'),
+                        },
+                    },
+                    {
+                        item: {
+                            id: 'stats',
+                            title: 'Статистика',
+                            icon: ChartColumn,
+                            // onItemClick: () =>
+                            //     setVisiblePanel(
+                            //         visiblePanel === Panel.Search ? undefined : Panel.Search,
+                            //     ),
+                        },
+                    },
+                ]}
+                renderContent={() => (
+                    <Wrapper>
+                        <InfoButtons />
+                    </Wrapper>
+                )}
+            />
+        </Routes>
     );
 };
 
---
+++ src/components/AsideHeader/AsideHeader.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+import './AsideHeader.css';
+
+export const InfoButtons: React.FC = () => {
+    return <div></div>;
+};
